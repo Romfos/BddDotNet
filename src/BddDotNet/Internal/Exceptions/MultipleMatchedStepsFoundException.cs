96d28cad2f1af1fddf6e678e75d0e2c9952da8f2
Romfos <Romfos@tut.by>
2026-08-20 13:22:15 +0200

    Update exception wording

--- src/BddDotNet/Internal/Exceptions/MultipleMatchedStepsFoundException.cs
+++ src/BddDotNet/Internal/Exceptions/MultipleMatchedStepsFoundException.cs
@@ -3,7 +3,7 @@ using BddDotNet.Steps;
 namespace BddDotNet.Internal.Exceptions;
 
 internal sealed class MultipleMatchedStepsFoundException(StepType stepType, string text)
-    : Exception($"Multiple matched steps found for {stepType} {text}")
+    : Exception($"Multiple matched steps found for `{stepType} {text}`")
 {
 }
 
--- src/BddDotNet/Internal/Exceptions/UnableToFindStepException.cs
+++ src/BddDotNet/Internal/Exceptions/UnableToFindStepException.cs
@@ -3,6 +3,6 @@ using BddDotNet.Steps;
 namespace BddDotNet.Internal.Exceptions;
 
 internal sealed class UnableToFindStepException(StepType stepType, string text)
-    : Exception($"Unable to find step to match {stepType} {text}")
+    : Exception($"Unable to find step to match `{stepType} {text}`")
 {
 }
